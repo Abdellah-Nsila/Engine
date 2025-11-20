a6b1330e97c8ae2ab361c5b54224e77a4358a32c
Abdellah-Nsila <dawndawn074@gmail.com>
2025-11-20 15:02:12 +0100

    Working on ImGui docking

---
+++ Engine/src/Engine/ImGui/ImGuiBuild.cpp
@@ -0,0 +1,4 @@
+#include "enginepch.h"
+
+#include "imgui/backends/imgui_impl_opengl3.cpp"
+#include "imgui/backends/imgui_impl_glfw.cpp"
--- Engine/src/Engine/ImGui/ImGuiLayer.h
+++ Engine/src/Engine/ImGui/ImGuiLayer.h
@@ -17,21 +17,14 @@ namespace Engine
 			ImGuiLayer();
 			~ImGuiLayer();
 
-			void	OnAttach();
-			void	OnDetach();
-			void	OnUpdate();
-			void	OnEvent(Event& event);
+			void	OnAttach() override;
+			void	OnDetach() override;
+			void	OnImGuiRender() override;
+
+			void	Begin();
+			void	End();
 
 			static ImGuiKey	MapToImGuiKey(int keycode);
-		private:
-			bool	OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-			bool	OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-			bool	OnMouseMovedEvent(MouseMovedEvent& e);
-			bool	OnMouseScrolledEvent(MouseScrolledEvent& e);
-			bool	OnKeyPressedEvent(KeyPressedEvent& e);
-			bool	OnKeyReleasedEvent(KeyReleasedEvent& e);
-			bool	OnKeyTypedEvent(KeyTypedEvent& e);
-			bool	OnWindowResizeEvent(WindowResizeEvent& e);
 		private:
 			float	m_Time = 0.0f;
 	};
